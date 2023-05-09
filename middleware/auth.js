f2e4389e7fd308beead513d1796c12e178648462
naufal assyafiq <assyafiqnaufal625@gmail.com>
2023-05-09 22:54:18 +0700

    berhasil login dan dapat JWT token

--- middleware/auth.js
+++ middleware/auth.js
@@ -43,3 +43,60 @@ exports.registrasi = function(req,res){
         }
     })
 }
+
+//controller untuk login
+exports.login = function(req,res){
+    var post = {
+        password: req.body.password,
+        eamil : req.body.eamil
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["user","password",md5(post.password),"eamil",post.eamil];
+
+    query = mysql.format(query,table);
+    connection.query(query,function(error,rows){
+        if(error){
+            console.log(error);
+        }else{
+            if(rows.length == 1){
+                var token = jwt.sign({rows},config.secret,{
+                    expiresIn: 1440
+                })
+                id_user = rows[0].id;
+
+                var data = {
+                    id_user: id_user,
+                    accsess_token: token,
+                    ip_adress: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["akses_token"];
+
+                query = mysql.format(query,table);
+                connection.query(query,data,function(error,rows){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        res.json({
+                            success: true,
+                            message:'Token JWT tergenerate',
+                            token: token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            }else{
+                res.json({"Error": true, "Message":"Email atau password salah!"})
+            }
+        }
+    });
+}
+
+
+
+
+
+
+
--- middleware/index.js
+++ middleware/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 //daftarkan menu registrasi
 router.post('/api/v1/register',auth.registrasi);
+router.post('/api/v1/login',auth.login);
 
 module.exports = router;
